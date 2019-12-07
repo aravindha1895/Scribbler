68494d85fa11e37eaa7943de55e7228aa36579eb
aravindha1895 <sridharanrvnd@gmail.com>
2019-12-07 20:12:22 +0530

    Javascript project card delete implementation

--- js/postslist.js
+++ js/postslist.js
@@ -1,5 +1,8 @@
-function openDeleteModel() {
+var activeCard;
+
+function openDeleteModel(index) {
     document.getElementById("confirmer-modal").style.display = "block";
+    activeCard = index;
 }
 
 function closeConfirmerDialog() {
@@ -8,5 +11,10 @@ function closeConfirmerDialog() {
 
 function deletePost(action) {
     document.getElementById("confirmer-modal").style.display = "none";
+    if (action == false) return;
     console.log(action);
+    var card = document.getElementById('post-card' + activeCard);
+    console.log(card);
+    card.style.display = "none";
+    card.className = "deleted";
 }
