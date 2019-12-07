6de7485b8ef0823b75d3c2d4d3a919940fd43fad
aravindha1895 <sridharanrvnd@gmail.com>
2019-12-07 21:21:14 +0530

    Comment button click hadler implementation

--- js/posts.js
+++ js/posts.js
@@ -2,6 +2,7 @@ var postTitile = "Every thing you should know about module and require in node.j
 var content = `Node.js is an open-source, cross-platform, JavaScript runtime environment that executes JavaScript code outside of a browser. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser. Consequently, Node.js represents a "JavaScript everywhere" paradigm,[6] unifying web-application development around a single programming language, rather than different languages for server- and client-side scripts.
 Though .js is the standard filename extension for JavaScript code, the name "Node.js" does not refer to a particular file in this context and is merely the name of the product. Node.js has an event-driven architecture capable of asynchronous I/O. These design choices aim to optimize throughput and scalability in web applications with many input/output operations, as well as for real-time Web applications (e.g., real-time communication programs and browser games)`;
 var likes = 0;
+var comments = [];
 
 function populatePost() {
     document.getElementById('post-contents').value = content;
@@ -19,7 +20,6 @@ function onEditButtnClicked() {
 }
 
 function onSaveButtonClicked() {
-    alert('HI');
     document.getElementById('edit-save-button').innerHTML = 'Edit <i class="fa fa-edit"></i>';
     document.getElementById('edit-save-button').setAttribute('onclick', 'onEditButtnClicked()');
     document.getElementById('post-contents').setAttribute('disabled', 'true');
@@ -32,4 +32,12 @@ function onLikesButtonCLicked() {
     likes++;
     document.getElementById('likesCount').innerHTML = likes + " person likes this!"
     document.getElementById('likeBtn').innerHTML = '<i class="fa fa-thumbs-up"></i>Liked';
+}
+
+function onCommentButtonClicked() {
+    console.log(comments);
+    var comment = document.getElementById('post-comment').value;
+    comments.unshift(comment);
+    console.log(comments);
+
 }
